33c422f888a3512d0ea1d04baee9aee36bb39172
Tinetirintine <jfontillaz.9008@gmail.com>
2026-05-20 13:45:13 +0800

    revision-update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.finalproject"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.finalproject"
